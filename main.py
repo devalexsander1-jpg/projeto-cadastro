2ec80bbcfb16662d5a8520bc3d972f243460e6c0
devalexsander1-jpg <dev.alexsander1@gmail.com>
2026-02-02 00:08:22 -0300

    modificação padrao do codigo, criei o loop e a lista vazia.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-#
+#criar um projeto de cadastro
 
+lista = []
+
+i = 0
+
+while True :
+	name = input("qual o nome do cliente: se quiser sair digite (sair) ")
+	lista.append(name)
+	if name == "sair":
+		break
+
+print(lista)
